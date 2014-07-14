e3e22b6244b69d6011368a68138b417857d6c4da
Brent Lanier <brent@lanier.us>
2014-07-14 16:39:43 -0400

    index.blade.php update

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -16,13 +16,13 @@ Programmer's Best Friend
         <legend>Programmer's Best Friend</legend>
 
         <h1>Lorem Ipsum Generator</h1>
-        <p>Creates <a href='/lorem-ipsum-generator'>random filler text<a/> for your applications.</p>
+        <p>Creates <a href='/lorem-ipsum-generator'>random filler text</a> for your applications.</p>
         <br>
         <h1>Random User Generator</h1>
-        <p>Creates <a href='/random-user-generator'>random user information<a/> for your applications.</p>
+        <p>Creates <a href='/random-user-generator'>random user information</a> for your applications.</p>
         <br>
         <h1>XKCD Password Generator</h1>
-        <p>Creates <a href='/xkcd-password-generator'>random strong passwords<a/> for user accounts.</p>
+        <p>Creates <a href='/xkcd-password-generator'>random strong passwords</a> for user accounts.</p>
 
     </fieldset>
 
